bc22c25dcb599f21823303d9fc5609af74ce69bb
Itai Steinherz <itaisteinherz@gmail.com>
2018-10-07 18:10:05 +0300

    Init

---
+++ extension/content.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const select = document.querySelector.bind(document);
+select.all = document.querySelectorAll.bind(document);
+
+const domLoaded = new Promise(resolve => {
+	setInterval(() => {
+		if (document.readyState === "complete") {
+			resolve();
+		}
+	}, 50);
+});
+
+const update = () => {
+	const enlargeRegExp = /^Unsubscribe/i;
+	const links = select.all("a");
+
+	if (links.length === 0) {
+		return;
+	}
+
+	const fragment = document.createDocumentFragment();
+	const enlarged = document.createElement("div");
+	enlarged.classList.add("unsubscribe-container");
+	fragment.appendChild(enlarged);
+
+	for (const link of links) {
+		if (enlargeRegExp.test(link.textContent)) {
+			const bigLink = link.cloneNode(true);
+			bigLink.style = {};
+			bigLink.classList.add("unsubscribe");
+			enlarged.appendChild(bigLink);
+		}
+	}
+
+	if (enlarged.children.length === 0) {
+		return;
+	}
+
+	select("div.nH.if").insertBefore(fragment, select("div.nH.aHU"));
+	createdButtons = true;
+};
+
+let createdButtons = false;
+let locationChanged = false;
+
+const init = () => {
+	update();
+
+	window.addEventListener("hashchange", () => { // Update on page change
+		if (createdButtons && !locationChanged) {
+			locationChanged = true;
+		} else if (!createdButtons || locationChanged) {
+			update();
+			createdButtons = true;
+			locationChanged = false;
+		}
+	});
+};
+
+domLoaded.then(init);
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Enlarge The Unsubscribe Button
+
+> Chrome extension - Enlarge the "unsubscribe" button in emails
+
+<a href="screenshot.png"><img src="screenshot.png" width="1107"></a>
+
+Inspired by [Bored Elon Musk](https://twitter.com/boredelonmusk):
+
+<a href="https://twitter.com/boredelonmusk/status/569551895053754368"><img src="tweet.png" width="476"></a>
+
+Currently only _Gmail_ is supported.
+
+
+## Install
+
+Install [manually](http://superuser.com/a/247654/6877).
+
+
+## License
+
+MIT © [Itai Steinherz](https://github.com/itaisteinherz)
